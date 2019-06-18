d9d4e9976412ccec26f414c88cae9ee6fd04cd13
Andrew Belousov <bedrew@yandex.ru>
2019-06-18 19:53:06 +0300

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,11 +1,11 @@
 const binarySearch = ( array, item ) => {
 
   let low = 0; 
-  let high = array.length
+  let high = array.length - 1
 
   while ( low <= high ) {
 
-    let mid = low + high
+    let mid = Math.round( ( low + high ) / 2 )
     let guess = array[mid]
 
     if ( guess == item ) {

c00986fcbc4a3ab8f52077da2aa1033dd5aee1ce
Andrew Belousov <bedrew@yandex.ru>
2019-07-15 21:00:52 +0300

    Create FindBrokenTag.js
    Одна из задач на собеседовании, задача звучала примерно так: 
    Напишите функцию которая проверит правильно ли вложены теги друг в друга. Если теги вложены не правильно вернуть первый не закрытый тег, если правильно то true

---
+++ FindBrokenTag.js
@@ -0,0 +1,39 @@
+function FindBrokenTag(str) { 
+ 
+  const findTags = str => {
+      let tags = []
+      str.split('<').forEach( temp => {
+          let tag = temp.split('>')[0]
+          if ( tag[0] ){  
+            tags.push(tag)
+          }
+      })
+      return tags
+  }
+  
+  const stackSort  = tags => {
+      let stack = []
+      
+      let stackPop = (arr, el) => {
+        if (arr.indexOf(el) != -1) arr.splice(arr.indexOf(el), 1);
+        if (arr.indexOf(el) != -1) arr.splice(arr.indexOf(el), 1);
+      }
+      
+      for ( let tag of tags ) {
+          stack.push(tag.replace('/',''))
+          if ( tag.includes('/') ) {
+             stackPop( stack, tag.replace('/',''))
+          }
+      }
+      return stack
+  }  
+
+  let result = stackSort( findTags( str ) )
+  
+  if ( result.length > 0 ) {
+    return result[0]
+  } else {
+    return true
+  }
+  
+}
